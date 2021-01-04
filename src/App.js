b7fda4aacdaf109db0d46ecaa14d796baf2f7d9b
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-01-04 21:50:37 +0530

    state keyword-for internal components

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  //state
+  state= {
+    counter : 101
+  }
   render() {
     return (
       <div className="App">
         <h1>React App</h1>
         <Person name='ABC' age='21'></Person>
         <Person name='gjh' age='25'>i like Cricket</Person>
-        <Person name='uoiuo' age='30'></Person>
+        <Person name='uoiuo' age='30'>{this.state.counter}</Person>
       </div>
     );
   }
