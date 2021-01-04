e53363bb0e2c1cadedee62bdb35cb18760f5c325
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-01-04 22:42:56 +0530

    set-state keyword-changes for internal components

--- src/App.js
+++ src/App.js
@@ -5,15 +5,37 @@ import Person from './Person/Person';
 class App extends Component {
   //state
   state= {
-    counter : 101
+    person : [
+      {name : 'Pulkit', age : '21'},
+      {name : 'Pankaj', age : '22'},
+      {name : 'Nitin', age : '23'}
+    ]
   }
+
+  //^ creating switch button function to execute the required task
+  switchNameHandler = () =>{
+    console.log('Executed on click')
+    //^set state feature in function for updating DOM
+
+    this.setState ({
+      person: [
+        {name : 'Nitin', age : 23},
+        {name : 'Pulkit', age : 21},
+        {name : 'Pankaj', age : 22},
+      ]
+    })
+
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React App</h1>
-        <Person name='ABC' age='21'></Person>
-        <Person name='gjh' age='25'>i like Cricket</Person>
-        <Person name='uoiuo' age='30'>{this.state.counter}</Person>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age}></Person>
+        <Person name={this.state.person[1].name} age={this.state.person[1].age}>i like Cricket</Person>
+        <Person name={this.state.person[2].name} age={this.state.person[2].age}>{this.state.counter}</Person>
+        
+        <button onClick = {this.switchNameHandler}> Switch Names </button>
       </div>
     );
   }
