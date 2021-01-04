ffbfaaf1e017a39a087a7f22d40a847b86f7a34d
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-01-04 23:30:28 +0530

    hooks - for functional components

--- src/App.js
+++ src/App.js
@@ -1,44 +1,95 @@
-import React, { Component } from 'react';
+// Function based React Components using Hooks
+
+
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  //state
-  state= {
+const App = props => {
+  //array destructuring
+  const [personState, setPersonState] = useState({
     person : [
-      {name : 'Pulkit', age : '21'},
-      {name : 'Pankaj', age : '22'},
-      {name : 'Nitin', age : '23'}
-    ]
-  }
+            {name : 'Pulkit', age : '21'},
+             {name : 'Pankaj', age : '22'},
+             {name : 'Nitin', age : '23'}
+           ]
+  });
+  console.log(personState);
 
-  //^ creating switch button function to execute the required task
-  switchNameHandler = () =>{
-    console.log('Executed on click')
-    //^set state feature in function for updating DOM
+  // another use state
+  const switchNameHandler = () =>{
+        console.log('Executed on click')
+    //     //^set state feature in function for updating DOM
+    
+        setPersonState ({
+          person: [
+            {name : 'Nitin', age : 23},
+            {name : 'Pulkit', age : 21},
+            {name : 'Pankaj', age : 22},
+          ]
+        });
+    
+      };
 
-    this.setState ({
-      person: [
-        {name : 'Nitin', age : 23},
-        {name : 'Pulkit', age : 21},
-        {name : 'Pankaj', age : 22},
-      ]
-    })
 
-  }
 
-  render() {
     return (
       <div className="App">
         <h1>React App</h1>
-        <Person name={this.state.person[0].name} age={this.state.person[0].age}></Person>
-        <Person name={this.state.person[1].name} age={this.state.person[1].age}>i like Cricket</Person>
-        <Person name={this.state.person[2].name} age={this.state.person[2].age}>{this.state.counter}</Person>
+        <Person name={personState.person[0].name} age={personState.person[0].age}></Person>
+        <Person name={personState.person[1].name} age={personState.person[1].age}>i like Cricket</Person>
+        <Person name={personState.person[2].name} age={personState.person[2].age}></Person>
         
-        <button onClick = {this.switchNameHandler}> Switch Names </button>
+        <button onClick = {switchNameHandler}> Switch Names </button>
       </div>
     );
   }
-}
 
 export default App;
+
+
+// Class based components
+// import React, { Component } from 'react';
+// import './App.css';
+// import Person from './Person/Person';
+
+// class App extends Component {
+//   //state
+//   state= {
+//     person : [
+//       {name : 'Pulkit', age : '21'},
+//       {name : 'Pankaj', age : '22'},
+//       {name : 'Nitin', age : '23'}
+//     ]
+//   }
+
+//   //^ creating switch button function to execute the required task
+//   switchNameHandler = () =>{
+//     console.log('Executed on click')
+//     //^set state feature in function for updating DOM
+
+//     this.setState ({
+//       person: [
+//         {name : 'Nitin', age : 23},
+//         {name : 'Pulkit', age : 21},
+//         {name : 'Pankaj', age : 22},
+//       ]
+//     })
+
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>React App</h1>
+//         <Person name={this.state.person[0].name} age={this.state.person[0].age}></Person>
+//         <Person name={this.state.person[1].name} age={this.state.person[1].age}>i like Cricket</Person>
+//         <Person name={this.state.person[2].name} age={this.state.person[2].age}>{this.state.counter}</Person>
+        
+//         <button onClick = {this.switchNameHandler}> Switch Names </button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
